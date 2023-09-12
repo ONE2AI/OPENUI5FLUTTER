ed86297110d8d0c8c97518d17f0472b94c54f123
ONE2AI <105135270+ONE2AI@users.noreply.github.com>
2023-09-12 14:40:57 +0530

    Update ui5_colors.dart

---
+++ UI5FLUTTER/utils/ui5_colors.dart
@@ -0,0 +1,13 @@
+part of ui5_flutter;
+
+class UI5Colors {
+  static const Color primary = Color(0xFF007ACC);
+  static const Color secondary = Color(0xFF6C757D);
+  static const Color success = Color(0xFF107C10);
+  static const Color error = Color(0xFFBB0000);
+  static const Color warning = Color(0xFFF0AB00);
+  static const Color information = Color(0xFF0072C6);
+  static const Color highlight = Color(0xFF6F2DA8);
+  static const Color neutral = Color(0xFF242424);
+  static const Color background = Color(0xFFF6F6F6);
+}
