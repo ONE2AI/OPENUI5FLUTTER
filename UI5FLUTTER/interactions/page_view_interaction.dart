0edb0d5c8747cfd840c28cb85fdd3966e5a67e1b
ONE2AI <105135270+ONE2AI@users.noreply.github.com>
2023-09-12 14:52:36 +0530

    Update page_view_interaction.dart

---
+++ UI5FLUTTER/interactions/page_view_interaction.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:ui5_flutter/ui5_flutter.dart';
+
+void main() {
+  runApp(UI5FLUTTER());
+}
+
+class UI5FLUTTER extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UI5FLUTTER',
+      theme: UI5Theme.lightTheme,
+      home: UI5FL
