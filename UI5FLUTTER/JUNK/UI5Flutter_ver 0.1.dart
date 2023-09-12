6d9dfddcaa8453690e318caedfa0122887794f7b
ONE2AI <105135270+ONE2AI@users.noreply.github.com>
2023-09-12 06:44:15 +0530

    Create UI5Flutter_ver 0.1.dart

---
+++ UI5FLUTTER/JUNK/UI5Flutter_ver 0.1.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(UI5InspiredApp());
+}
+
+class UI5InspiredApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UI5 Inspired App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Segoe UI', // You can use a UI5-inspired font here
+      ),
+      home: UI5InspiredHomePage(),
+    );
+  }
+}
+
+class UI5InspiredHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('UI5 Inspired App'),
+      ),
+      body: Center(
+        child: UI5InspiredCard(),
+      ),
+    );
+  }
+}
+
+class UI5InspiredCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(16.0),
+      elevation: 4.0,
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Hello, UI5!',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            RaisedButton(
+              onPressed: () {
+                // Handle button press
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text('Button Clicked!'),
+                    content: Text('You clicked the button.'),
+                    actions: [
+                      FlatButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('OK'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: Text('Click Me'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
