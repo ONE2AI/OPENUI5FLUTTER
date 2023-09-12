7f07e9cefefedbad2d91d1e418be87d97c815027
ONE2AI <105135270+ONE2AI@users.noreply.github.com>
2023-09-12 15:00:15 +0530

    Create ui5_elements.dart

---
+++ UI5FLUTTER/ui5_elements.dart
@@ -0,0 +1,18 @@
+library ui5_elements;
+
+import 'package:flutter/material.dart';
+
+part 'src/buttons.dart';
+part 'src/cards.dart';
+part 'src/typography.dart';
+part 'src/dialogs.dart';
+part 'src/bottom_sheets.dart';
+// Add other UI5-like elements and functionalities as separate parts
+
+// Export public elements
+export 'src/buttons.dart';
+export 'src/cards.dart';
+export 'src/typography.dart';
+export 'src/dialogs.dart';
+export 'src/bottom_sheets.dart';
+// Export other UI5-like elements and functionalities
