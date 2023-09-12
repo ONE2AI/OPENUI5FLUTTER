d8e725a9e0f093caf18974add7021cb3239a6c9e
ONE2AI <105135270+ONE2AI@users.noreply.github.com>
2023-09-12 06:46:58 +0530

    Create UI5Flutter_Ver 0.2.dart

---
+++ UI5FLUTTER/JUNK/UI5Flutter_Ver 0.2.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(UI5InspiredApp());
+}
+
+class UI5InspiredApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UI5 Inspired App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Segoe UI', // You can use a UI5-inspired font here
+      ),
+      home: UI5InspiredHomePage(),
+    );
+  }
+}
+
+class UI5InspiredHomePage extends StatelessWidget {
+  final List<String> sampleItems = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('UI5 Inspired App'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: sampleItems.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(sampleItems[index]),
+                  leading: Icon(Icons.check_circle),
+                );
+              },
+            ),
+          ),
+          UI5InspiredFooter(),
+        ],
+      ),
+    );
+  }
+}
+
+class UI5InspiredFooter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      color: Colors.blue[50], // UI5-inspired footer color
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          RaisedButton(
+            onPressed: () {
+              // Handle navigation button press
+              // For demonstration purposes, simply show a snackbar
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Navigate Back pressed'),
+                ),
+              );
+            },
+            child: Text('Navigate Back'),
+          ),
+          RaisedButton(
+            onPressed: () {
+              // Handle navigation button press
+              // For demonstration purposes, simply show a snackbar
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Navigate Forward pressed'),
+                ),
+              );
+            },
+            child: Text('Navigate Forward'),
+          ),
+        ],
+      ),
+    );
+  }
+}
