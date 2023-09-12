4888fc5d0e2f776f9e876bb0a0dfbc19695f4dc8
ONE2AI <105135270+ONE2AI@users.noreply.github.com>
2023-09-12 14:41:49 +0530

    Update ui5_data_model.dart

---
+++ UI5FLUTTER/models/ui5_data_model.dart
@@ -0,0 +1,33 @@
+part of ui5_flutter;
+
+class Product {
+  final int id;
+  final String name;
+  final String description;
+  final double price;
+  final String imageUrl;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+  });
+}
+
+class User {
+  final int id;
+  final String firstName;
+  final String lastName;
+  final String email;
+
+  User({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+  });
+}
+
+// Add more data models as needed for your app
