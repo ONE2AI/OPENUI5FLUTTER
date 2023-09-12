171b47f62ae5d7273d15382b11fee5b0dc43e164
ONE2AI <105135270+ONE2AI@users.noreply.github.com>
2023-09-12 14:03:55 +0530

    Create UI5InspiredApp_Dialog.dart

---
+++ UI5FLUTTER/JUNK/UI5InspiredApp_Dialog.dart
@@ -0,0 +1,103 @@
+///Below is an example code that demonstrates the implementation of AlertDialog and SimpleDialog in Flutter
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(UI5InspiredApp());
+}
+
+class UI5InspiredApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UI5 Inspired App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Segoe UI', // You can use a UI5-inspired font here
+      ),
+      home: UI5InspiredHomePage(),
+    );
+  }
+}
+
+class UI5InspiredHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('UI5 Inspired App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                _showAlertDialog(context);
+              },
+              child: Text('Show AlertDialog'),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                _showSimpleDialog(context);
+              },
+              child: Text('Show SimpleDialog'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showAlertDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Alert Dialog'),
+          content: Text('This is an AlertDialog example.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showSimpleDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: Text('Simple Dialog'),
+          children: [
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Option 1'),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Option 2'),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Option 3'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
