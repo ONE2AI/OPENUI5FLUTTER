f8aa974cdf809b7685b35801657bfab4930b318a
ONE2AI <105135270+ONE2AI@users.noreply.github.com>
2023-09-12 14:04:57 +0530

    Create UI5InspiredApp_BottomNavigator.dart

---
+++ UI5FLUTTER/JUNK/UI5InspiredApp_BottomNavigator.dart
@@ -0,0 +1,65 @@
+///Below is an example code that demonstrates the implementation of BottomNavigationBar in Flutter
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(UI5InspiredApp());
+}
+
+class UI5InspiredApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UI5 Inspired App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Segoe UI', // You can use a UI5-inspired font here
+      ),
+      home: UI5InspiredHomePage(),
+    );
+  }
+}
+
+class UI5InspiredHomePage extends StatefulWidget {
+  @override
+  _UI5InspiredHomePageState createState() => _UI5InspiredHomePageState();
+}
+
+class _UI5InspiredHomePageState extends State<UI5InspiredHomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('UI5 Inspired App'),
+      ),
+      body: Center(
+        child: Text('Welcome to UI5 Inspired App!'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
